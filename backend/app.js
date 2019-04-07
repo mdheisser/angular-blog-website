4c790966ab308bc2f8fdf76ad18d2964c747f914
TopTalentDev <toptal715@gmail.com>
2019-04-07 22:09:11 +0530

    fixing some path issues

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
